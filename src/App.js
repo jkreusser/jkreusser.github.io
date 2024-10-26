e69f5f09a8edff9c7ac87ca2feab64c3eea90735
kreusser <109634560+jkreusser@users.noreply.github.com>
2024-10-27 01:50:47 +0200

    Navigation Update

---
+++ src/App.js
@@ -0,0 +1,209 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Header from './components/Header';
+import Imprint from './components/Imprint';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Service from './components/Service';
+import Profile from './components/Profile';
+import Work from './components/Work';
+import Process from './components/Process';
+import PersonaKit from './work/PersonaKit';
+import DentaltechnikWeber from './work/DentaltechnikWeber';
+import ShapeUp from './work/ShapeUp';
+import Lottie from 'lottie-react';
+import logoAnimation from './lottie/logoAnimation.json';
+import { useState, useEffect } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+
+function App() {
+  const { t } = useTranslation();
+  const location = useLocation();
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [showAnimation, setShowAnimation] = useState(true); // Zustand für die Logo-Animation
+  const [isHomepage, setIsHomepage] = useState(true); // Zustand für Homepage-Status
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setIsHomepage(true); // Setze isHomepage auf true, wenn die aktuelle Route die Homepage ist
+    } else {
+      setIsHomepage(false); // Setze isHomepage auf false, wenn die aktuelle Route nicht die Homepage ist
+    }
+
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1)); // Hash ohne #
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+      setShowAnimation(false); // Animation nicht anzeigen, wenn zu IDs navigiert wird
+    } else {
+      window.scrollTo(0, 0);
+      setShowAnimation(true); // Animation anzeigen bei Seitenwechsel
+    }
+  }, [location]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFirstLoad(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="App flex flex-col items-center w-full">
+      <Navigation />
+      <TransitionGroup className="w-full">
+        <CSSTransition
+          key={location.key}
+          timeout={1100}
+          classNames="black-screen"
+        >
+          {state => (
+            <div className="relative w-full h-full">
+              <Routes location={location} className="w-full">
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('home-seo-title')}</title>
+                        <meta name="description" content={t('home-seo-description')} />
+                        <meta property="og:title" content={t('home-seo-title')} />
+                        <meta property="og:description" content={t('home-seo-description')} />
+                        <meta property="og:image" content="/android-chrome-512x512.png" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/" />
+                      </Helmet>
+                      <Header />
+                      <main>
+                        <Work />
+                        <Process />
+                        <Profile />
+                        <Service />
+                      </main>
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+                <Route
+                  path="/imprint"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('imprint-seo-title')}</title>
+                        <meta name="description" content={t('imprint-seo-description')} />
+                        <meta property="og:title" content={t('imprint-seo-title')} />
+                        <meta property="og:description" content={t('imprint-seo-description')} />
+                        <meta property="og:image" content="/android-chrome-512x512.png" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/imprint" />
+                      </Helmet>
+                      <Imprint />
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+                <Route
+                  path="/privacy-policy"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('privacy-seo-title')}</title>
+                        <meta name="description" content={t('privacy-seo-description')} />
+                        <meta property="og:title" content={t('privacy-seo-title')} />
+                        <meta property="og:description" content={t('privacy-seo-description')} />
+                        <meta property="og:image" content="/android-chrome-512x512.png" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/privacy-policy" />
+                      </Helmet>
+                      <PrivacyPolicy />
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+                <Route
+                  path="/personakit"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('personakit-seo-title')}</title>
+                        <meta name="description" content={t('personakit-seo-description')} />
+                        <meta property="og:title" content={t('personakit-seo-title')} />
+                        <meta property="og:description" content={t('personakit-seo-description')} />
+                        <meta property="og:image" content="/personakit/personakit_cover.webp" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/personakit" />
+                      </Helmet>
+                      <PersonaKit />
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+                <Route
+                  path="/dentaltechnik-weber"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('weber-seo-title')}</title>
+                        <meta name="description" content={t('weber-seo-description')} />
+                        <meta property="og:title" content={t('weber-seo-title')} />
+                        <meta property="og:description" content={t('weber-seo-description')} />
+                        <meta property="og:image" content="/weber/weber_cover.webp" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/dentaltechnik-weber" />
+                      </Helmet>
+                      <DentaltechnikWeber />
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+                <Route
+                  path="/shape-up"
+                  element={
+                    <>
+                      <Helmet>
+                        <title>{t('shapeup-seo-title')}</title>
+                        <meta name="description" content={t('shapeup-seo-description')} />
+                        <meta property="og:title" content={t('shapeup-seo-title')} />
+                        <meta property="og:description" content={t('shapeup-seo-description')} />
+                        <meta property="og:image" content="/shapeup/shapeup_cover.webp" />
+                        <link rel="canonical" href="https://www.joschka-kreusser.de/shape-up" />
+                      </Helmet>
+                      <ShapeUp />
+                      <section className='w-full bg-white'>
+                        <Footer />
+                      </section>
+                    </>
+                  }
+                />
+              </Routes>
+              {(firstLoad || state === 'entering' || state === 'exiting') && showAnimation && !isHomepage && ( // Animation nur anzeigen, wenn showAnimation true ist und nicht auf Homepage
+                <div className="fixed inset-0 bg-black flex items-center justify-center z-50">
+                  <Lottie animationData={logoAnimation} style={{ height: '200px', width: '200px' }} />
+                </div>
+              )}
+            </div>
+          )}
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+}
+
+export default function Root() {
+  return (
+    <HelmetProvider>
+      <Router>
+        <App />
+      </Router>
+    </HelmetProvider>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const Header = () => {
+    const { t } = useTranslation();
+
+    const headerText = t('header').split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+            {line}
+            <br />
+        </React.Fragment>
+    ));
+
+    return (
+        <section className='max-w-screen-2xl mx-auto flex flex-col justify-center my-32 px-8 md:px-16 mt-24 mb-32'>
+            <span className='text-[2rem] leading-[2rem] sm:text-[3rem] sm:leading-[3rem] md:text-[4rem] md:leading-[4rem] lg:text-[5rem] lg:leading-[5rem] xl:text-[6rem] xl:leading-[6rem] 2xl:text-[9rem] 2xl:leading-[9rem]'>{headerText}</span>
+            {/*
+                    <Link>
+                        <div className='hover-path flex justify-center items-center w-24 h-24 hover:border hover:border-black hover:bg-white bg-black rounded-full'>
+                            <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 2H37V38" stroke="white" stroke-width="3" />
+                                <path d="M1.72015 37.28L37.0002 2" stroke="white" stroke-width="3" />
+                            </svg>
+                        </div>
+                    </Link>
+                    */}
+            <div className='mt-8 flex justify-end'> <p className='md:max-w-[387px] uppercase leading-6'>{t('specialization')}</p></div>
+        </section >
+    );
+}
+
+export default Header;
